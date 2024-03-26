0d8a5e947b51cff4a67e9ff19622a549f7bf0cb7
Juhwan01 <wnghks5432@naver.com>
2024-03-26 23:30:55 +0900

    0326
    바구니

--- Assets/scripts/select.cs
+++ Assets/scripts/select.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class select : MonoBehaviour
 {
-    Button[] btn;
-    public static int cnt;
+    private List<Button> clickedButtons = new List<Button>();
 
-    public void checkButton()
+    public Button[] buttons;
+    public int num;
+    private List<int> loNum = new List<int>();
+    private Button lastClickedButton;
+
+    void Start()
     {
-        if (cnt > 4)
+        for (int i = 0; i < buttons.Length; i++)
         {
-            Debug.Log("»çÅÁ ´Ù °í¸§¤»");
+            Button button = buttons[i];
+            button.onClick.AddListener(() => Click(button));
+        }
+    }
+
+    public void Click(Button clickedButton)
+    {
+        if (clickedButtons.Contains(clickedButton))
+        {
+            clickedButton.image.color = new Color(255f, 255f, 255f, 1f);
+            clickedButtons.Remove(clickedButton);
+        }
+        else
+        {
+            clickedButton.image.color = new Color(255f, 255f, 255f, 0.4f);
+            clickedButtons.Add(clickedButton);
+        }
+
+        PerformActionForFiveClickedButtons();
+        lastClickedButton = clickedButton;
+    }
+    private void PerformActionForFiveClickedButtons()
+    {
+        if (clickedButtons.Count >= num)
+        {
+            foreach (Button button in buttons)
+            {
+                if (!clickedButtons.Contains(button))
+                {
+                    button.interactable = false;
+                }
+            }
+        }
+        else
+        {
+            foreach (Button button in buttons)
+            {
+                button.interactable = true;
+            }
         }
     }
 }
