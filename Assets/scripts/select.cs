0d8df337b65a6602f0138c2b13058ca3c1ff0c1e
Juhwan01 <wnghks5432@naver.com>
2024-03-27 18:42:02 +0900

    0327
    animation

--- Assets/scripts/select.cs
+++ Assets/scripts/select.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Linq;
+using NUnit.Framework;
+using System;
 
 public class select : MonoBehaviour
 {
     private List<Button> clickedButtons = new List<Button>();
 
     public Button[] buttons;
+    public Button[] cButtons;
     public int num;
     private List<int> loNum = new List<int>();
-    private Button lastClickedButton;
-
+    private Animator anim;
+    public int[] indexPos;
+    public Button submitBtn;
     void Start()
     {
         for (int i = 0; i < buttons.Length; i++)
@@ -20,23 +25,39 @@ public class select : MonoBehaviour
             Button button = buttons[i];
             button.onClick.AddListener(() => Click(button));
         }
+        for (int i = 0; i < cButtons.Length; i++)
+        {
+            Button button = cButtons[i];
+            button.onClick.AddListener(() => backClick(button));
+        }
+        indexPos = new int[5];
     }
 
     public void Click(Button clickedButton)
     {
-        if (clickedButtons.Contains(clickedButton))
-        {
-            clickedButton.image.color = new Color(255f, 255f, 255f, 1f);
-            clickedButtons.Remove(clickedButton);
-        }
-        else
-        {
-            clickedButton.image.color = new Color(255f, 255f, 255f, 0.4f);
-            clickedButtons.Add(clickedButton);
-        }
-
+        clickedButtons.Add(clickedButton);
+        for(int i=0;i<cButtons.Length; i++)
+            if (!cButtons[i].interactable)
+            {
+                cButtons[i].image.color = new Color(255f, 255f, 255f, 1f);
+                cButtons[i].interactable = true;
+                indexPos[i] = Array.IndexOf(buttons, clickedButton);
+                break;
+            }     
+        clickedButton.image.color = new Color(255f, 255f, 255f, 0f);
+        clickedButton.interactable = false;
         PerformActionForFiveClickedButtons();
-        lastClickedButton = clickedButton;
+    }
+    public void backClick(Button clickedButton)
+    {
+        int index = Array.IndexOf(cButtons, clickedButton);
+        buttons[indexPos[index]].image.color = new Color(255f, 255f, 255f, 1f);
+        clickedButton.image.color = new Color(255f, 255f, 255f, 0f);
+        buttons[indexPos[index]].interactable = true;
+        clickedButton.interactable = false;
+        clickedButtons.Remove(buttons[indexPos[index]]);
+        Debug.Log(index);
+        PerformActionForFiveClickedButtons();
     }
     private void PerformActionForFiveClickedButtons()
     {
@@ -46,16 +67,20 @@ public class select : MonoBehaviour
             {
                 if (!clickedButtons.Contains(button))
                 {
+                    
                     button.interactable = false;
                 }
             }
+            submitBtn.interactable = true;
         }
         else
         {
             foreach (Button button in buttons)
             {
+                
                 button.interactable = true;
             }
+            submitBtn.interactable = false;
         }
     }
 }
